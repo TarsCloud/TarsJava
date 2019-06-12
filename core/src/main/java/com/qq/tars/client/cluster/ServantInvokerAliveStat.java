f0637d02204b9725cebac945ad2b5370ccf86bd4
XenoAmess <xenoamess@gmail.com>
2019-06-13 05:35:40 +0800

    use BigDecimal.valueOf(v1) instead of new BigDecimal(Double.toString(v1)) for better readability.

--- core/src/main/java/com/qq/tars/client/cluster/ServantInvokerAliveStat.java
+++ core/src/main/java/com/qq/tars/client/cluster/ServantInvokerAliveStat.java
@@ -147,8 +147,8 @@ public class ServantInvokerAliveStat {
         if (scale < 0) {
             throw new IllegalArgumentException("The scale must be a positive integer or zero");
         }
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
         return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 }
