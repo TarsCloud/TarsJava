a62a31e2b3381f9e409da08853e3485346b369e4
Timmy <yuweitaocn@gmail.com>
2020-01-09 11:10:07 +0800

    [IMPLEMENT] When a startup error is encountered, output the current error and stop the application from being pulled by the client

---
+++ spring/tars-spring-boot-starter/src/main/java/com/qq/tars/spring/bean/ApplicationFailListener.java
@@ -0,0 +1,45 @@
+package com.qq.tars.spring.bean;
+
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+public class ApplicationFailListener implements SpringApplicationRunListener {
+    @Override
+    public void starting() {
+
+    }
+
+    @Override
+    public void environmentPrepared(ConfigurableEnvironment environment) {
+
+    }
+
+    @Override
+    public void contextPrepared(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void contextLoaded(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void started(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void running(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void failed(ConfigurableApplicationContext context, Throwable exception) {
+        exception.printStackTrace();
+        System.out.println("[TARS]  start application fail" + exception.getMessage());
+        System.exit(-1);
+    }
+
+}
