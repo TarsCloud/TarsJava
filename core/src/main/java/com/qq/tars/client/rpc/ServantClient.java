06da120fb08108f8aa2e4a57537edddc683e9efd
邹佳敏 <jaminzou@upchina.com>
2020-03-23 11:56:59 +0800

    fix invokeWithFuture Null Exception

--- core/src/main/java/com/qq/tars/client/rpc/ServantClient.java
+++ core/src/main/java/com/qq/tars/client/rpc/ServantClient.java
@@ -180,7 +180,7 @@ public class ServantClient {
         try {
             ensureConnected();
             request.setInvokeStatus(InvokeStatus.FUTURE_CALL);
-            ticket = TicketManager.createTicket(request, session, this.syncTimeout, callback);
+            ticket = TicketManager.createTicket(request, session, this.asyncTimeout, callback, selectorManager);
 
             Session current = session;
             current.write(request);
