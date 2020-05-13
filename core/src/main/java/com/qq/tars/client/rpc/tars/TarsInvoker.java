66f0e1ae079deeab06803fa0161bffb811395a37
Timmy <yuweitaocn@gmail.com>
2020-05-13 23:38:10 +0800

    [fix bug]  When there are no callback. There is no need to re-construct callbackWarp

--- core/src/main/java/com/qq/tars/client/rpc/tars/TarsInvoker.java
+++ core/src/main/java/com/qq/tars/client/rpc/tars/TarsInvoker.java
@@ -192,8 +192,9 @@ public class TarsInvoker<T> extends ServantInvoker<T> {
             request.setStatus(status);
 
         }
-        FilterChain filterChain = new TarsClientFilterChain(filters, objName, FilterKind.CLIENT, client, InvokeStatus.ASYNC_CALL,
-                new TarsCallbackWrapper(config, request.getFunctionName(), getUrl().getHost(), getUrl().getPort(), request.getBornTime(), request, callback, this));
+        TarsCallbackWrapper tarsCallbackWrapper = callback == null ?
+                null : new TarsCallbackWrapper(config, request.getFunctionName(), getUrl().getHost(), getUrl().getPort(), request.getBornTime(), request, callback, this);
+        FilterChain filterChain = new TarsClientFilterChain(filters, objName, FilterKind.CLIENT, client, InvokeStatus.ASYNC_CALL, tarsCallbackWrapper);
         filterChain.doFilter(request, response);
     }
 
