1e6abb7a060f1fd90e771fe9d891cc21f637ca94
Timmy <yuweitaocn@gmail.com>
2020-06-17 22:39:52 +0800

    [update]update license

--- core/src/main/java/com/qq/tars/protocol/tars/support/TarsPromiseFutureCallback.java
+++ core/src/main/java/com/qq/tars/protocol/tars/support/TarsPromiseFutureCallback.java
@@ -1,3 +1,18 @@
+/**
+ * Tencent is pleased to support the open source community by making Tars available.
+ * <p>
+ * Copyright (C) 2016 THL A29 Limited, a Tencent company. All rights reserved.
+ * <p>
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * <p>
+ * https://opensource.org/licenses/BSD-3-Clause
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package com.qq.tars.protocol.tars.support;
 
 
@@ -16,10 +31,7 @@ import org.slf4j.Logger;
 
 import java.util.concurrent.CompletableFuture;
 
-/**
- * jce的CompletableFuture的实现
- * @author Kerrigan
- */
+
 public class TarsPromiseFutureCallback<V> implements Callback<TarsServantResponse> {
     private static final Logger logger = LoggerFactory.getClientLogger();
 
