f18d66448157fcd91d74948dbcb70d05b30c6040
luxiaopi <luxiaobohit@hotmail.com>
2020-07-19 18:42:00 +0800

    feat:support tars-json for protobuf-server

--- protobuf/src/main/java/com/qq/tars/rpc/protocol/proto/ProtoCodec.java
+++ protobuf/src/main/java/com/qq/tars/rpc/protocol/proto/ProtoCodec.java
@@ -17,6 +17,8 @@
 package com.qq.tars.rpc.protocol.proto;
 
 import com.google.protobuf.Message;
+import com.google.protobuf.MessageOrBuilder;
+import com.google.protobuf.util.JsonFormat;
 import com.qq.tars.common.util.Constants;
 import com.qq.tars.net.protocol.ProtocolException;
 import com.qq.tars.protocol.tars.support.TarsMethodInfo;
@@ -25,8 +27,12 @@ import com.qq.tars.protocol.util.TarsHelper;
 import com.qq.tars.rpc.protocol.tars.TarsCodec;
 import com.qq.tars.rpc.protocol.tars.TarsServantRequest;
 import com.qq.tars.rpc.protocol.tars.TarsServantResponse;
+import com.qq.tars.rpc.protocol.tars.support.AnalystManager;
 
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Map;
 
 public class ProtoCodec extends TarsCodec {
     public ProtoCodec(String charsetName) {
@@ -46,6 +52,41 @@ public class ProtoCodec extends TarsCodec {
     }
 
     @Override
+    protected byte[] encodeJsonResult(TarsServantResponse response, String charsetName) {
+        TarsServantRequest request = response.getRequest();
+        if (TarsHelper.isPing(request.getFunctionName())) {
+            return new byte[]{};
+        }
+
+        // 服务端接口响应
+        String result = "{}"; // initial empty json result
+        int ret = response.getRet();
+        Map<String, TarsMethodInfo> methodInfoMap;
+        if (ret == TarsHelper.SERVERSUCCESS &&
+                (methodInfoMap = AnalystManager.getInstance().getMethodMapByName(request.getServantName())) != null) {
+            TarsMethodInfo methodInfo = methodInfoMap.get(request.getFunctionName());
+            TarsMethodParameterInfo returnInfo = methodInfo.getReturnInfo();
+            if (returnInfo != null && returnInfo.getType() != Void.TYPE && response.getResult() != null) {
+                try {
+                    String protobufJsonResult = JsonFormat.printer().print((MessageOrBuilder) response.getResult());
+                    // 拼接tars_ret JSON结果
+                    result = "{\"tars_ret\": " + protobufJsonResult + "}";
+                } catch (Exception e) {
+                    System.err.println("server encode json ret :" + response.getResult() + ", with ex:" + e);
+                }
+            }
+        }
+
+        try {
+            return result.getBytes(charsetName);
+        } catch (UnsupportedEncodingException e) {
+            System.err.println("server encode json encode :" + result + ", with charset:" + charsetName
+                    + ", with ex: " + e);
+            return new byte[]{};
+        }
+    }
+
+    @Override
     protected byte[] encodeRequestParams(TarsServantRequest request, String charsetName) throws ProtocolException {
         Object[] parameter = request.getMethodParameters();
         if (TarsHelper.isCallback(request.getMethodInfo().getParametersList().get(0).getAnnotations())) {
@@ -58,6 +99,17 @@ public class ProtoCodec extends TarsCodec {
     protected Object[] decodeRequestBody(byte[] data, String charset,  TarsMethodInfo methodInfo) throws Exception {
         List<TarsMethodParameterInfo> parametersList = methodInfo.getParametersList();
         return new Object[]{MethodCache.getInstance().parseFrom((Class) parametersList.get(0).getStamp(), data)};
+    }
+
+    @SuppressWarnings({"unchecked"})
+    @Override
+    protected Object[] decodeRequestJsonBody(byte[] data, String charset, TarsMethodInfo methodInfo) throws Exception {
+        List<TarsMethodParameterInfo> parametersList = methodInfo.getParametersList();
+        Class protobufParamClass = (Class) parametersList.get(0).getStamp();
+        Method newBuilder = protobufParamClass.getDeclaredMethod("newBuilder");
+        Message.Builder builder = (Message.Builder) newBuilder.invoke(protobufParamClass);
+        JsonFormat.parser().ignoringUnknownFields().merge(new String(data, charset), builder);
+        return new Object[]{builder.build()};
     }
 
     @Override
