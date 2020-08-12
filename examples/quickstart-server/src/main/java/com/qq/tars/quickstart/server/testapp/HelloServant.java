a70ad506b4c26f52d8c9dd54d3e13cf7cadbcea5
邹佳敏 <jaminzou@upchina.com>
2020-08-12 15:57:29 +0800

    fixed example servant to support http+json protocol

--- examples/quickstart-server/src/main/java/com/qq/tars/quickstart/server/testapp/HelloServant.java
+++ examples/quickstart-server/src/main/java/com/qq/tars/quickstart/server/testapp/HelloServant.java
@@ -13,9 +13,4 @@ import com.qq.tars.common.support.Holder;
 public interface HelloServant {
 
 	 String hello(@TarsMethodParameter(name="no")int no, @TarsMethodParameter(name="name")String name);
-<<<<<<< HEAD
-
-	 int helloJson(@TarsMethodParameter(name="tie")TestInfo tie, @TarsHolder(name="otie") Holder<TestInfoEx> otie);
-=======
->>>>>>> c003611... fixed example servant to support http+json protocol
 }
