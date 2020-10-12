f7d68dbc9abba8be5ca39a95ec08b5a7ece0a780
liyuqi <liyuqi@yuewen.com>
2020-10-12 19:03:37 +0800

    [change]  LoggerFactory support log4j2
    * adapter log4j2 root name

--- tars-logger/tars-logger-log4j2/src/main/java/com/qq/tars/logger/LoggerFactoryLog4j2Impl.java
+++ tars-logger/tars-logger-log4j2/src/main/java/com/qq/tars/logger/LoggerFactoryLog4j2Impl.java
@@ -3,15 +3,18 @@ package com.qq.tars.logger;
 import com.qq.tars.common.logger.LoggerFactory;
 import com.qq.tars.server.config.ConfigurationManager;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.slf4j.event.Level;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Collection;
 
 public class LoggerFactoryLog4j2Impl implements LoggerFactory {
-
     private static final String NAME_LOGGER_FILE = System.getProperty("tars.logger.log4j2.name", "log4j2.xml");
+    private static final String NAME_ROOT_LOGGER = org.apache.logging.log4j.LogManager.ROOT_LOGGER_NAME;
+    private static final String NAME_ROOT_LOGGER_SLF = org.slf4j.Logger.ROOT_LOGGER_NAME;
 
     @Override
     public int reloadConfig() {
@@ -36,7 +39,22 @@ public class LoggerFactoryLog4j2Impl implements LoggerFactory {
     @Override
     public int setLoggerLevel(String logName, Level level) {
         final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-        ctx.getLogger(logName).setLevel(org.apache.logging.log4j.Level.valueOf(level.name()));
+        org.apache.logging.log4j.Level newLevel = org.apache.logging.log4j.Level.valueOf(level.name());
+
+        if (logName.equalsIgnoreCase(NAME_ROOT_LOGGER_SLF)) {
+            //adapter SLF root logger name
+            logName = NAME_ROOT_LOGGER;
+        }
+        Logger logger = ctx.getLogger(logName);
+        logger.setLevel(newLevel);
+
+        Collection<Logger> loggers = ctx.getLoggers();
+        for (Logger lg : loggers) {
+            if (lg.isAdditive() && logger.equals(lg.getParent())) {
+                lg.setLevel(newLevel);
+            }
+        }
+
         return 0;
     }
 
