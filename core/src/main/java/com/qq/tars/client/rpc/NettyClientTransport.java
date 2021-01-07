9dcda51387bd61c6ec8d75b7fdf5d5d8ead3dafb
Timmy <yuweitaocn@gmail.com>
2021-01-07 11:41:12 +0800

    [add] use default config

--- core/src/main/java/com/qq/tars/client/rpc/NettyClientTransport.java
+++ core/src/main/java/com/qq/tars/client/rpc/NettyClientTransport.java
@@ -17,10 +17,17 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.timeout.IdleStateHandler;
+import io.netty.util.concurrent.DefaultThreadFactory;
+
+import java.util.concurrent.ThreadFactory;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class NettyClientTransport {
+    private static final int DEFAULT_IO_THREADS = Math.min(Runtime.getRuntime().availableProcessors() + 1, 32);
+
+    private static final EventLoopGroup EVENT_LOOP_GROUP = getNioEventLoopGroup();
+
     private Bootstrap bootstrap;
     private final ServantProxyConfig servantProxyConfig;
 
@@ -31,18 +38,27 @@ public class NettyClientTransport {
 
     private ChannelHandler channelHander;
 
+    private static EventLoopGroup getNioEventLoopGroup() {
+        ThreadFactory threadFactory = new DefaultThreadFactory("netty-client-worker", true);
+        if (Epoll.isAvailable()) {
+            return new EpollEventLoopGroup(DEFAULT_IO_THREADS, threadFactory);
+        } else if (KQueue.isAvailable()) {
+            return new KQueueEventLoopGroup(DEFAULT_IO_THREADS, threadFactory);
+        } else {
+            return new NioEventLoopGroup(DEFAULT_IO_THREADS, threadFactory);
+        }
+    }
+
+
     public void init() {
         bootstrap = new Bootstrap();
-        EventLoopGroup myEventLoopGroup;
-        if (KQueue.isAvailable()) {
-            myEventLoopGroup = new KQueueEventLoopGroup(4);
-            bootstrap.group(myEventLoopGroup).channel(KQueueSocketChannel.class);
-        } else if (Epoll.isAvailable()) {
-            myEventLoopGroup = new EpollEventLoopGroup(4);
-            bootstrap.group(myEventLoopGroup).channel(EpollSocketChannel.class);
+        bootstrap.group(getNioEventLoopGroup());
+        if (Epoll.isAvailable()) {
+            bootstrap.channel(EpollSocketChannel.class);
+        } else if (KQueue.isAvailable()) {
+            bootstrap.channel(KQueueSocketChannel.class);
         } else {
-            myEventLoopGroup = new NioEventLoopGroup(4);
-            bootstrap.group(myEventLoopGroup).channel(NioSocketChannel.class);
+            bootstrap.channel(NioSocketChannel.class);
         }
         bootstrap.option(ChannelOption.SO_KEEPALIVE, true).option(ChannelOption.TCP_NODELAY, true)
                 .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
@@ -54,8 +70,8 @@ public class NettyClientTransport {
                 IdleStateHandler clientIdleHandler =
                         new IdleStateHandler(0, servantProxyConfig.getConnectTimeout(), 0, MILLISECONDS);
                 ChannelPipeline p = ch.pipeline();
-                p.addLast("encoder", new TarsEncoder("UTF-8"))
-                        .addLast("decoder", new TarsDecoder("UTF-8"))
+                p.addLast("encoder", new TarsEncoder(servantProxyConfig.getCharsetName()))
+                        .addLast("decoder", new TarsDecoder(servantProxyConfig.getCharsetName()))
                         .addLast("idle", clientIdleHandler)
                         .addLast("handler", new NettyClientHandler(channelHander, servantProxyConfig))
                 ;
--- core/src/test/java/com/qq/tars/TransportTest.java
+++ core/src/test/java/com/qq/tars/TransportTest.java
@@ -13,6 +13,8 @@ import com.qq.tars.protocol.util.TarsHelper;
 import com.qq.tars.rpc.protocol.tars.TarsServantRequest;
 import com.qq.tars.rpc.protocol.tars.TarsServantResponse;
 import com.qq.tars.rpc.protocol.tars.support.AnalystManager;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import org.junit.Test;
 
@@ -45,7 +47,7 @@ public class TransportTest {
 
             @Override
             public void received(Channel channel, Object message) {
-                System.out.println("receive message " + ((TarsServantResponse)message).getResult());
+                System.out.println("receive message " + ((TarsServantResponse) message).getResult());
 
             }
 
@@ -63,57 +65,50 @@ public class TransportTest {
         NettyClientTransport nettyClientTransport = new NettyClientTransport(servantProxyConfig, channelHandler);
         nettyClientTransport.init();
         NettyServantClient client = nettyClientTransport.connect("10.172.0.111", 18393);
-
-        MonitorQueryReq monitorQueryReq = new MonitorQueryReq();
-        monitorQueryReq.dateType = DateType.MINIUES.value();
-        monitorQueryReq.method = "query";
-        monitorQueryReq.setDataid("tars_stat");
-        monitorQueryReq.intervalTime = 1;
-        monitorQueryReq.setConditions(ImmutableList.of(new Condition("slave_name", 5, "tars.tarsstat")));
-        monitorQueryReq.indexs = ImmutableList.of("succ_count", "timeout_count", "exce_count", "total_time");
-        monitorQueryReq.setGroupby(ImmutableList.of("f_tflag"));
-        monitorQueryReq.startTime = 1609171222L;
-        monitorQueryReq.endTime = 1609221622L;
-
-        TarsServantRequest tarsServantRequest = new TarsServantRequest();
-
-
-        Method method = MonitorQueryPrx.class.getMethod("query", MonitorQueryReq.class, Holder.class);
-        System.out.println(method);
-
-        tarsServantRequest.setMethodInfo(AnalystManager.getInstance().getMethodMap(MonitorQueryPrx.class).get(method));
-
-
-        tarsServantRequest.setCharsetName("UTF-8");
-        tarsServantRequest.setServantName("tars.tarsquerystat.QueryObj");
-        tarsServantRequest.setInvokeStatus(Request.InvokeStatus.SYNC_CALL);
-        Holder<MonitorQueryRsp> response = new Holder<>();
-        tarsServantRequest.setMethodParameters(new Object[]{monitorQueryReq, response});
-        tarsServantRequest.setCharsetName("UTF-8");
-        tarsServantRequest.setApi(MonitorQueryPrx.class);
-        tarsServantRequest.setFunctionName("query");
-        tarsServantRequest.setMessageType(TarsHelper.MESSAGETYPENULL);
-        tarsServantRequest.setVersion(TarsHelper.VERSION);
-        tarsServantRequest.setContext(new HashMap<>());
-        tarsServantRequest.setStatus(new HashMap<>());
-        tarsServantRequest.setPacketType(TarsHelper.NORMAL);
-
-        System.out.println(new Object[]{monitorQueryReq, response});
-
-        Thread.sleep(100);
+        for (int i = 0; i < 2; i++) {
+            MonitorQueryReq monitorQueryReq = new MonitorQueryReq();
+            monitorQueryReq.dateType = DateType.MINIUES.value();
+            monitorQueryReq.method = "query";
+            monitorQueryReq.setDataid("tars_stat");
+            monitorQueryReq.intervalTime = 1;
+            monitorQueryReq.setConditions(ImmutableList.of(new Condition("slave_name", 5, "tars.tarsstat")));
+            monitorQueryReq.indexs = ImmutableList.of("succ_count", "timeout_count", "exce_count", "total_time");
+            monitorQueryReq.setGroupby(ImmutableList.of("f_tflag"));
+            monitorQueryReq.startTime = 1609171222L;
+            monitorQueryReq.endTime = 1609221622L;
+            TarsServantRequest tarsServantRequest = new TarsServantRequest();
+            Method method = MonitorQueryPrx.class.getMethod("query", MonitorQueryReq.class, Holder.class);
+            System.out.println(method);
+            tarsServantRequest.setMethodInfo(AnalystManager.getInstance().getMethodMap(MonitorQueryPrx.class).get(method));
+            tarsServantRequest.setCharsetName("UTF-8");
+            tarsServantRequest.setServantName("tars.tarsquerystat.QueryObj");
+            tarsServantRequest.setInvokeStatus(Request.InvokeStatus.SYNC_CALL);
+            Holder<MonitorQueryRsp> response = new Holder<>();
+            tarsServantRequest.setMethodParameters(new Object[]{monitorQueryReq, response});
+            tarsServantRequest.setCharsetName("UTF-8");
+            tarsServantRequest.setApi(MonitorQueryPrx.class);
+            tarsServantRequest.setFunctionName("query");
+            tarsServantRequest.setMessageType(TarsHelper.MESSAGETYPENULL);
+            tarsServantRequest.setVersion(TarsHelper.VERSION);
+            tarsServantRequest.setContext(new HashMap<>());
+            tarsServantRequest.setStatus(new HashMap<>());
+            tarsServantRequest.setPacketType(TarsHelper.NORMAL);
+            System.out.println(new Object[]{monitorQueryReq, response});
+            System.out.println("client connection is " + client.getChannel().isActive());
+            System.out.println("client connection is " + client.getChannel().isOpen());
+            System.out.println("client connection is " + client.getChannel().isWritable());
+            long startTime = System.currentTimeMillis();
+            CompletableFuture<TarsServantResponse> responseCompletableFuture = client.send(tarsServantRequest).whenComplete((x, y) -> {
+                System.out.print(System.currentTimeMillis() - startTime + "::");
+                System.out.println(response);
+            });
+        }
         System.out.println("client connection is " + client.getChannel().isActive());
         System.out.println("client connection is " + client.getChannel().isOpen());
         System.out.println("client connection is " + client.getChannel().isWritable());
 
-        CompletableFuture<TarsServantResponse> responseCompletableFuture = client.send(tarsServantRequest);
 
-        System.out.println("client connection is " + client.getChannel().isActive());
-        System.out.println("client connection is " + client.getChannel().isOpen());
-        System.out.println("client connection is " + client.getChannel().isWritable());
-        System.out.println(responseCompletableFuture.get().getResult());
-        System.out.println(response);
-
-
+        Thread.sleep(50000);
     }
 
 
@@ -138,6 +133,21 @@ public class TransportTest {
         System.out.println(monitorQueryReq.getIndexs());
         System.out.println(monitorQueryReq.getConditions());
         System.out.println("request2" + request2);
+
+    }
+
+
+    @Test
+    public void maxCappcity() {
+        ByteBuf buffer = Unpooled.directBuffer();
+        buffer.writeInt(2014);
+        buffer.writeInt(2014);
+        buffer.writeInt(2014);
+        buffer.writeInt(2014);
+        buffer.writeInt(2014);
+        System.out.println(buffer.getInt(4 * 5));
+
+
     }
 
 }
