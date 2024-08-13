ba69a1de9e9e707938667c74db8fd3fbf3af5721
Timmy <yuweitaocn@gmail.com>
2024-08-13 17:36:38 +0800

    Squashed commit of the following:
    commit 6949a37fba005210201597a24836fc7b1caca0d2
    Merge: 57fac98 90000a5
    Author: zhangyaoshu <shuzhangyao@163.com>
    Date:   Mon Aug 5 16:45:28 2024 +0800
    
        Merge remote-tracking branch 'origin/v1.7.x' into v1.7.x
    
    commit 57fac98ae4ec64a9b8dc3153857b047f40f02b93
    Author: zhangyaoshu <shuzhangyao@163.com>
    Date:   Mon Aug 5 15:52:53 2024 +0800
    
        当client端是TarsHttpService服务时，调用链上报的 tracer 设置为Application.Server。

--- core/src/main/java/com/qq/tars/support/trace/TraceClientFilter.java
+++ core/src/main/java/com/qq/tars/support/trace/TraceClientFilter.java
@@ -4,6 +4,7 @@ import com.qq.tars.common.ClientVersion;
 import com.qq.tars.common.Filter;
 import com.qq.tars.common.FilterChain;
 import com.qq.tars.common.util.Constants;
+import com.qq.tars.common.util.StringUtils;
 import com.qq.tars.context.DistributedContext;
 import com.qq.tars.context.DistributedContextManager;
 import com.qq.tars.net.core.Request;
@@ -15,6 +16,7 @@ import com.qq.tars.server.config.ConfigurationManager;
 import com.qq.tars.server.config.ServerConfig;
 import io.opentracing.Scope;
 import io.opentracing.Tracer;
+import io.opentracing.log.Fields;
 import io.opentracing.propagation.Format;
 import io.opentracing.propagation.TextMapInjectAdapter;
 import io.opentracing.tag.Tags;
@@ -24,88 +26,103 @@ import java.util.Map;
 
 public class TraceClientFilter implements Filter {
 
-	private boolean isTrace = false;
+    private boolean isTrace = false;
 
-	@Override
-	public void init() {
-		isTrace = ConfigurationManager.getInstance().getServerConfig().getSampleRate() > 0;
-	}
+    @Override
+    public void init() {
+        isTrace = ConfigurationManager.getInstance().getServerConfig().getSampleRate() > 0;
+    }
 
-	@Override
-	public void doFilter(Request request, Response response, FilterChain chain) throws Throwable {
-		if (!isTrace) {
-			chain.doFilter(request, response);
-			return;
-		}
-		if (!(request instanceof TarsServantRequest) || !TraceUtil.checkServant(((TarsServantRequest)request).getServantName())) {
-			chain.doFilter(request, response);
-			return;
-		}
-		ServerConfig config = ConfigurationManager.getInstance().getServerConfig();
-		DistributedContext context = DistributedContextManager.getDistributedContext();
-		String servantName = context.get(TraceManager.INTERNAL_SERVANT_NAME);
-		Tracer tracer = TraceContext.getInstance().getCurrentTracer();
-		
-        if (tracer == null) {
-        	chain.doFilter(request, response);
-        } else {
-        	TarsServantRequest tarsServantRequest = (TarsServantRequest)request;
-        	boolean isSync = tarsServantRequest.getInvokeStatus() == InvokeStatus.SYNC_CALL 
-        			|| tarsServantRequest.getInvokeStatus() == InvokeStatus.FUTURE_CALL;
-        	
-        	String protocol = Constants.TARS_PROTOCOL;
-        	Map<String, String> requestContext = tarsServantRequest.getContext();
-        	if (requestContext != null && !requestContext.isEmpty()) {
-        		protocol = requestContext.get(TraceManager.PROTOCOL);
-        		requestContext.remove(TraceManager.PROTOCOL);
-        	}
-        	try(Scope scope = tracer.buildSpan(tarsServantRequest.getFunctionName()).withTag(Tags.SPAN_KIND.getKey(), Tags.SPAN_KIND_CLIENT).startActive(isSync)) {
-        		Map<String, String> status = tarsServantRequest.getStatus();
-        		if (status == null) {
-        			tarsServantRequest.setStatus(new HashMap<String, String>());
-        			status = tarsServantRequest.getStatus();
-        		}
-        		tracer.inject(scope.span().context(), Format.Builtin.TEXT_MAP, new TextMapInjectAdapter(status));
-        		scope.span().setTag("client.ipv4", config.getLocalIP());
-        		scope.span().setTag("client.port", config.getServantAdapterConfMap().get(servantName).getEndpoint().port());
-        		scope.span().setTag("tars.interface", getObjName(servantName));
-        		scope.span().setTag("tars.method", tarsServantRequest.getFunctionName());
-        		scope.span().setTag("tars.protocol", protocol);
-        		scope.span().setTag("tars.client.version", ClientVersion.getVersion());
-        		
-        		
-        		TarsServantResponse tarsServantResponse = (TarsServantResponse)response;
-        		try {
-        			chain.doFilter(request, response);
-        			if (isSync) {
-            			scope.span().setTag("tars.retcode", Integer.toString(tarsServantResponse.getRet()));
-            		} else {
-            			TraceManager.getInstance().putSpan(request.getTicketNumber(), tracer, scope.span());
-            		}
-        		} catch (Exception e) {
-        			scope.span().log(e.getMessage());
-        			throw e;
-        		}
-        		
-        		
-        	}
+    @Override
+    public void doFilter(Request request, Response response, FilterChain chain) throws Throwable {
+        if (!isTrace) {
+            chain.doFilter(request, response);
+            return;
         }
-	}
+        if (!(request instanceof TarsServantRequest) || !TraceUtil.checkServant(((TarsServantRequest) request).getServantName())) {
+            chain.doFilter(request, response);
+            return;
+        }
+        ServerConfig config = ConfigurationManager.getInstance().getServerConfig();
+        DistributedContext context = DistributedContextManager.getDistributedContext();
+        String servantName = context.get(TraceManager.INTERNAL_SERVANT_NAME);
+        Tracer tracer = TraceContext.getInstance().getCurrentTracer();
+        TraceContext traceContext = null;
+        if (tracer == null) {
+            traceContext = TraceContext.getInstance().initCurrentTrace(config.getApplication() + "." + config.getServerName());
+            tracer = TraceContext.getInstance().getCurrentTracer();
+        }
+        try {
+            if (tracer == null) {
+                chain.doFilter(request, response);
+            } else {
+                TarsServantRequest tarsServantRequest = (TarsServantRequest) request;
+                boolean isSync = tarsServantRequest.getInvokeStatus() == InvokeStatus.SYNC_CALL
+                        || tarsServantRequest.getInvokeStatus() == InvokeStatus.FUTURE_CALL;
 
-	@Override
-	public void destroy() {
+                String protocol = Constants.TARS_PROTOCOL;
+                Map<String, String> requestContext = tarsServantRequest.getContext();
+                if (requestContext != null && !requestContext.isEmpty()) {
+                    protocol = requestContext.get(TraceManager.PROTOCOL);
+                    requestContext.remove(TraceManager.PROTOCOL);
+                }
+                try (Scope scope = tracer.buildSpan(tarsServantRequest.getFunctionName()).withTag(Tags.SPAN_KIND.getKey(), Tags.SPAN_KIND_CLIENT).startActive(isSync)) {
+                    Map<String, String> status = tarsServantRequest.getStatus();
+                    if (status == null) {
+                        tarsServantRequest.setStatus(new HashMap<String, String>());
+                        status = tarsServantRequest.getStatus();
+                    }
+                    tracer.inject(scope.span().context(), Format.Builtin.TEXT_MAP, new TextMapInjectAdapter(status));
+                    scope.span().setTag("client.ipv4", config.getLocalIP());
+                    if(config.getServantAdapterConfMap().containsKey(servantName)){
+                        scope.span().setTag("client.port", config.getServantAdapterConfMap().get(servantName).getEndpoint().port());
+                    }
+                    if(StringUtils.isNotEmpty(servantName)){
+                        scope.span().setTag("tars.interface", getObjName(servantName));
+                    }
+                    scope.span().setTag("tars.method", tarsServantRequest.getFunctionName());
+                    scope.span().setTag("tars.protocol", protocol);
+                    scope.span().setTag("tars.client.version", ClientVersion.getVersion());
+                    TarsServantResponse tarsServantResponse = (TarsServantResponse) response;
+                    try {
+                        chain.doFilter(request, response);
+                        if (isSync) {
+                            scope.span().setTag("tars.retcode", Integer.toString(tarsServantResponse.getRet()));
+                        } else {
+                            TraceManager.getInstance().putSpan(request.getTicketNumber(), tracer, scope.span());
+                        }
+                    } catch (Exception e) {
+                        Tags.ERROR.set(scope.span(), true);
+                        Map<String, Object> logs = new HashMap<>();
+                        logs.put(Fields.EVENT, "error");
+                        logs.put(Fields.ERROR_OBJECT, e);
+                        logs.put(Fields.STACK, e.getStackTrace());
+                        scope.span().log(logs);
+                        throw e;
+                    }
+                }
+            }
+        } finally {
+            if (traceContext != null) {
+                traceContext.close();
+            }
+        }
+    }
 
-	}
-	
-	private String getObjName(String fullName) {
-		if (fullName == null || fullName.length() == 0) {
-			return null;
-		}
-		String[] parts = fullName.split("\\.");
-		if (parts.length < 3) {
-			return fullName;
-		}
-		return parts[2];
-	}
+    @Override
+    public void destroy() {
+
+    }
+
+    private String getObjName(String fullName) {
+        if (fullName == null || fullName.length() == 0) {
+            return null;
+        }
+        String[] parts = fullName.split("\\.");
+        if (parts.length < 3) {
+            return fullName;
+        }
+        return parts[2];
+    }
 
 }
